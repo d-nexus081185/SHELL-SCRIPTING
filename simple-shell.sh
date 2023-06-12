b3d26cc0c1485da3df28c5bfadd475709d072b72
nexus <emekaezeobi@outlook.com>
2023-06-12 11:37:46 +0000

    Made changes to the dashboard code

--- simple-shell.sh
+++ simple-shell.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/bash
-echo "TO ACCESS YOUR DASHBOARD, ENTER USERNAME & PASSWORD"
+echo "TO ACCESS YOUR DASHBOARD, ENTER USERNAME & DEFAULT PASSWORD"
 
+echo "Enter Your Desired User Name"
 read -p "Enter Your Username: " user_name
+echo "Enter Default Password provided by the ADMIN"
 password_number=12345
-read -sp "Enter Your Password: " user_password
+read -sp "Enter Your Default Password: " default_password
 echo
-if [ "$user_password" -eq "$password_number" ]; then
+if [ "$default_password" -eq "$password_number" ]; then
 echo "Password match found. Please change your password."
 read -sp "Please Change Your Password: " new_password
 echo
 echo "Password Changed Successfully" 
 else
-echo "Password does not match"
+echo "Wrong Default Password Entered!"
+read -p "Enter Correct Default Password: " default_password1
+echo "ACCESS DENIED!! Contact the ADMIN for the Default Password!"
 fi
