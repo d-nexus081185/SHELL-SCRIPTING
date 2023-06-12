2f0566ef47588acfe06e249868dc0aef16d2887d
nexus <emekaezeobi@outlook.com>
2023-06-12 10:08:22 +0000

    New update

--- shell-test.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/bash
-
-echo "Welcome to dual boot"
---
+++ simple-shell.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+echo Welcome to your dashboard
+
+read -p 'Enter Username: ' user_var
+read -sp 'Enter Password: ' pass_var
+echo
+
+
