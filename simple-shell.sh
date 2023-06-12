b2f117201d176592f7110ca5d3a4cb109fb94465
nexus <emekaezeobi@outlook.com>
2023-06-12 10:55:30 +0000

    Simple Shell for dashboard access

--- simple-shell.sh
+++ simple-shell.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/bash
+echo "TO ACCESS YOUR DASHBOARD, ENTER USERNAME & PASSWORD"
 
-echo Welcome to your dashboard
-
-read -p 'Enter Username: ' user_var
-read -sp 'Enter Password: ' pass_var
+read -p "Enter Your Username: " user_name
+password_number=12345
+read -sp "Enter Your Password: " user_password
 echo
-
-
+if [ "$user_password" -eq "$password_number" ]; then
+echo "Password match found. Please change your password."
+read -sp "Please Change Your Password: " new_password
+echo
+echo "Password Changed Successfully" 
+else
+echo "Password does not match"
+fi
