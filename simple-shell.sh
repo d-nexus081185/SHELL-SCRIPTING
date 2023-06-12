4535d314b2ea3dd0fdeacefaae378822ee97b3e5
nexus <emekaezeobi@outlook.com>
2023-06-12 16:10:12 +0100

    Recent Update

--- simple-shell.sh
+++ simple-shell.sh
@@ -7,19 +7,22 @@ read -p "Enter Your Desired Username: " user_name
 echo "Enter Default Password provided by the ADMIN"
 password_number=12345
 read -sp "Enter Your Default Password: " default_password
-echo
+echo 
+echo "Welcome $user_name, Default Password matched. Please change your password"
 
 sleep 3
 
 if [ "$default_password" -eq "$password_number" ]; then
-echo "Password match found. Please change your password."
+
 read -sp "Please Enter New Password: " new_password
 echo
-read -sp "Please Confirm New Password: "
+read -sp "Please Confirm New Password: " confirm_password
 echo
-echo "Password Changed Successfully"
-else
-echo "Wrong Default Password Entered"
-sleep 2
-echo "ACCESS DENIED!!! Contact the ADMIN!!!"
 fi
+	if [ "$new_password" -eq "$confirm_password" ]; then
+	echo "Congratulations! Password changed was successful"
+	else
+	echo "Wrong Confirmation Password Entered!"
+	sleep 2
+	echo "ACCESS DENIED!!! Contact the ADMIN."
+	fi
