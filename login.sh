a9d53bd86a073ab3e30d003894e100d80d89803b
nexus <emekaezeobi@outlook.com>
2023-06-13 15:44:13 +0100

    git commit 13-06_15:44

--- login.sh
+++ login.sh
@@ -8,13 +8,24 @@ default_password=12345
 read -p "Username: " username
 read -sp "Password: " password
 echo
+read -p "Email: " email
 
 #Compare the user input and declared variables
 if [[ $username == $default_username && $password == $default_password ]]; then
-echo "Login successful! Redirecting to website"
+echo "Login successful! Please change your Password"
 sleep 2
-open "https://google.com"
+#open "https://google.com"
 else
 echo "Access denied! Login failed."
 fi
-
+read -sp "Change Pasword: " new_password
+echo
+read -sp "Confirm Password: " confirm_password
+echo
+echo
+if [[ $new_password == $confirm_password ]]; then
+echo "Password successfully changed, Check email for login details"
+#echo "$message" | username=$default_username password=$new_password -s "$email"
+else
+echo "Password Mismatch! Access Denied, contact the Admin" 
+fi
