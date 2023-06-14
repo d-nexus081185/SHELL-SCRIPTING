d47cee6c4039b88ba8838ed55128558fd3e60717
nexus <emekaezeobi@outlook.com>
2023-06-14 14:11:19 +0100

    Added some lines and made some changes

--- functions/age.sh
+++ functions/age.sh
@@ -2,18 +2,23 @@
 
 #Declarations
 current_year=2023
-
+echo "-----------------------"
+echo "ZOBI AGE PREDICTOR APP"
+echo "-----------------------"
+echo
 #User-Input Section
 read -sp "Please Enter Your Birth Year: " birth_year
 echo
-age=$((current_year - birth_year))
-echo "You were born in the year $birth_year"
+echo "Hey! No secrets here! You were born in the year $birth_year"
 sleep 2
 echo "If that is true, you should be.."
+sleep 3
 echo
+echo "Thinking..."
 sleep 3
-echo "Calculating..."
-sleep 3
+
+age=$((current_year - birth_year))
+
 #Conditionals
 if [[ $birth_year -le $current_year ]]; then
 echo "$age years old"
