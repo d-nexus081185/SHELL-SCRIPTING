2b7636c3b3846ffcd0b6551eba2fe638a5f030be
nexus <emekaezeobi@outlook.com>
2023-06-14 13:16:31 +0100

    edited the age code

--- functions/age.sh
+++ functions/age.sh
@@ -4,12 +4,19 @@
 current_year=2023
 
 #User-Input Section
-read -p "Birth Year: " birth_year
+read -sp "Please Enter Your Birth Year: " birth_year
+echo
 age=$((current_year - birth_year))
-
+echo "You were born in the year $birth_year"
+sleep 2
+echo "If that is true, you should be.."
+echo
+sleep 3
+echo "Calculating..."
+sleep 3
 #Conditionals
 if [[ $birth_year -le $current_year ]]; then
-echo "You are $age years old"
+echo "$age years old"
 else 
 echo "Are you an alien?"
 fi
