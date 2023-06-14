f1f45b7ca45eaa2f1065f355c8da64ea66c1d38e
nexus <emekaezeobi@outlook.com>
2023-06-14 14:16:34 +0100

    More commitment for improved code

--- functions/age.sh
+++ functions/age.sh
@@ -6,12 +6,15 @@ echo "-----------------------"
 echo "ZOBI AGE PREDICTOR APP"
 echo "-----------------------"
 echo
+sleep 2
 #User-Input Section
 read -sp "Please Enter Your Birth Year: " birth_year
+echo
 echo
 echo "Hey! No secrets here! You were born in the year $birth_year"
 sleep 2
 echo "If that is true, you should be.."
+echo
 sleep 3
 echo
 echo "Thinking..."
