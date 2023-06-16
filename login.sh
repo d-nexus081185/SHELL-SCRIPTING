aac70cdc3ea3bc7d028f31046b5dff363ffbb07e
nexus <emekaezeobi@outlook.com>
2023-06-16 09:06:14 +0100

    16-06-23 09:06 upload

--- login.sh
+++ login.sh
@@ -3,7 +3,7 @@
 #Declaration of the default username and password
 default_username=admin
 default_password=12345
-set -n
+
 #User input section
 read -p "Username: " username
 read -sp "Password: " password
@@ -15,15 +15,15 @@ sleep 2
 #open "https://google.com"
 else
 echo "Access denied! Login failed."
+break
 fi
-#read -sp "Change Pasword: " new_password
-#echo
-#read -sp "Confirm Password: " confirm_password
-#echo
-#echo
-#if [[ $new_password == $confirm_password ]]; then
-#echo "Password successfully changed, Check email for login details"
-#echo "$message" | username=$default_username password=$new_password -s "$email"
-#else
-#echo "Password Mismatch! Access Denied, contact the Admin" 
-#fi
+read -sp "Change Pasword: " new_password
+echo
+read -sp "Confirm Password: " confirm_password
+echo
+echo
+if [[ $new_password == $confirm_password ]]; then
+echo "Password successfully changed, Check email for login details"
+else
+echo "Password mismatched, Conatct system Admin"
+fi
